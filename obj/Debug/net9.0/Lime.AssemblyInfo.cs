f07cf1c4adef425cb338bf4034d2f53255a180cd
Kisonix-Dev <kisonix.tv@mail.ru>
2025-02-23 18:11:10 +0300

    - Added logo for Readme.md.

--- obj/Debug/net9.0/Lime.AssemblyInfo.cs
+++ obj/Debug/net9.0/Lime.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7f58b72ad9d402ec1e7067e352d1a0526631573")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74a326bd76158216ea6bb99420be6a13dcf09f34")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
